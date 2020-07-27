f4e0fcd97e00c4a2c2f6085b7377f1f924c6328d
Colby Peyton <colby.peyton@gmail.com>
2020-07-26 21:24:35 -0500

    Home screen is completed. Added images folder containing all of the necessary images. Continue work on the about/menu screens

--- src/home.js
+++ src/home.js
@@ -1,14 +1,15 @@
 class Home{
     createHeading(){
         const h1 = document.createElement('h1');
-        h1.textContent = 'Welcome To Odin\'s';
+        h1.textContent = 'Odin\'s';
+        h1.classList.add('heading');
         return h1;
     }
 
     createBody(){
         const div = document.createElement('div');
         div.classList.add('container-body')
-        div.innerHTML = '<p>Where the food and drinks are Godly</p>';
+        div.innerHTML = '<p>Food and drinks fit <span>for a God<span></p>';
 
         return div;
     }
@@ -19,13 +20,17 @@ class Home{
 
     appendElementsForDisplay(){
         const container = document.createElement('div');
+        container.classList.add('home');
         container.classList.add('container');
+        const div = document.createElement('div');
+        div.classList.add('container-color');
         const heading = this.createHeading();
         const body = this.createBody();
 
 
-        container.appendChild(heading);
-        container.appendChild(body);
+        div.appendChild(heading);
+        div.appendChild(body);
+        container.appendChild(div);
         return container;
     }
 
@@ -34,8 +39,3 @@ class Home{
 const home = new Home();
 
 export default home;
-
-
-{/* <h1>Welcome to Odin's</h1>
-<p>Where the food is godly</p>
-<img src="https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80"/> */}
